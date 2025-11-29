f27d68ab9ab4a92ff980f3e855f6e0f0271c19b3
yoniafek <37255810+yoniafek@users.noreply.github.com>
2025-11-29 15:26:53 -0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Google Maps API - Using the free tier */}
+        {/* Google Maps API with Places library */}
         <script
           src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
           async
